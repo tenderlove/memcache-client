eed041e6e46da39bda3f8648cb0a024bd99dee55
Mike Perham <mperham@gmail.com>
2009-07-20 10:15:38 -0500

    Latest 1.9.1+ don't need custom rbuf_fill anymore

--- lib/memcache.rb
+++ lib/memcache.rb
@@ -1039,17 +1039,17 @@ class MemCache
   class BufferedIO < Net::BufferedIO # :nodoc:
     BUFSIZE = 1024 * 16
 
-    # An implementation similar to this is in *trunk* for 1.9.  When it
-    # gets released, this method can be removed when using 1.9
-    def rbuf_fill
-      begin
-        @rbuf << @io.read_nonblock(BUFSIZE)
-      rescue Errno::EWOULDBLOCK
-        retry unless @read_timeout
-        if IO.select([@io], nil, nil, @read_timeout)
-          retry
-        else
-          raise Timeout::Error, 'IO timeout'
+    if RUBY_VERSION < '1.9.1'
+      def rbuf_fill
+        begin
+          @rbuf << @io.read_nonblock(BUFSIZE)
+        rescue Errno::EWOULDBLOCK
+          retry unless @read_timeout
+          if IO.select([@io], nil, nil, @read_timeout)
+            retry
+          else
+            raise Timeout::Error, 'IO timeout'
+          end
         end
       end
     end
