2c5ed63027227626830d9686f14f3870d778e0a9
Mike Perham <mperham@gmail.com>
2010-03-18 22:33:18 -0500

    Change systemtimer load logic to exclude MacRuby, Rubinius, etc.

--- lib/memcache.rb
+++ lib/memcache.rb
@@ -10,16 +10,16 @@ require 'net/protocol'
 
 begin
   # Try to use the SystemTimer gem instead of Ruby's timeout library
-  # when running on something that looks like Ruby 1.8.x. See:
+  # when running on Ruby 1.8.x. See:
   #   http://ph7spot.com/articles/system_timer
-  # We don't want to bother trying to load SystemTimer on jruby and
-  # ruby 1.9+
-  if defined?(JRUBY_VERSION) || (RUBY_VERSION >= '1.9')
-    require 'timeout'
-    MemCacheTimer = Timeout
-  else
+  # We don't want to bother trying to load SystemTimer on jruby,
+  # ruby 1.9+ and rbx.
+  if !defined?(RUBY_ENGINE) || (RUBY_ENGINE == 'ruby' && RUBY_VERSION < '1.9.0')
     require 'system_timer'
     MemCacheTimer = SystemTimer
+  else
+    require 'timeout'
+    MemCacheTimer = Timeout
   end
 rescue LoadError => e
   puts "[memcache-client] Could not load SystemTimer gem, falling back to Ruby's slower/unsafe timeout library: #{e.message}"
